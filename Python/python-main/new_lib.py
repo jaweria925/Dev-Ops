9a8d09b60c4b680b5bfe6fe191dc4a10531951c9
jaweria <jaweriaaslam1@gmail.com>
2025-04-17 11:31:43 +1000

    add file

--- Python/python-main/new_lib.py
+++
@@ -1,8 +0,0 @@
-import requests
-r = requests.get("https://gitlab.com/api/v4/users/jaweriaaslam317/projects")
-my_project = r.json()
-print(my_project)
-
-#print(type(my_project))
-for project in my_project:
-    print(f"project name is: {project['name']}\n project_url: {project['http_url_to_repo']} \n")
